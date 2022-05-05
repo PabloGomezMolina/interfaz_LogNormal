995432a3a9f980c9c207e49a70b0b2debd4ff027
laquers <laquers@gmail.com>
2022-05-05 12:27:20 -0400

    agregado lineas de codigo para el problema de pyinstaller

--- interfaz/interfaz.py
+++ interfaz/interfaz.py
@@ -11,7 +11,19 @@ import numpy as np
 import random
 import pandas as pd
 from scipy.stats import lognorm
+import os, sys
 
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for Pyinstaller """
+    try:
+        #Pyinstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    
+    return os.path.join(base_path, relative_path)
+    
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
